bec9ac823fe41dfc496a8b01ddc7eca0596cec57
Delia Șușcă <deliasusca@yahoo.ro>
2021-10-08 11:43:32 +0300

    Am rezolvat problema 13

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
-def get_newton_sqrt(n,steps):
-    '''
-    :param n:numarul caruia ii calculam radicalul
-    :param steps: numarul de pasi pentru calcularea radicalului
-    :return: radicalul
-    '''
-    a=n
-    x0=2;
-    for i in range (steps):
-        #x_(n+1)=0.5*(x_n+a/value)*0.5
-        if i != 0 : n=(n+ a/n)*0.5
-        else : n= (x0+a/x0)*0.5
-    return float(n)
 
-def test_get_newton_sqrt():
-    assert get_newton_sqrt(4,2)==2
-    assert get_newton_sqrt(9, 2)>3 and get_newton_sqrt(9, 2)<3.1
-    assert get_newton_sqrt(15, 2)>3.9 and get_newton_sqrt(15, 2)<4
-    print(get_newton_sqrt(9,2))
-    print(get_newton_sqrt(15,2))
+def get_temp(temp: float, fromt: str, to: str) -> float:
+    '''
+    :param temp: Temperatura introdusa
+    :param fromt: Scara in care este temperatura introdusa
+    :param to: Scara in care vom converti temperatura introdusa
+    :return: Temperatura convertita in scara ceruta
+    '''
+    if fromt == 'C':
+        if to == 'K':
+            return temp + 273.15
+        elif to == 'F':
+            return temp*1.8 + 32
+    elif fromt == 'K':
+        if to == 'C':
+            return temp - 273.15
+        elif to == 'F':
+            return (temp-32)/1.8 + 273.15
+    elif fromt == 'F':
+        if to == 'C':
+            return (temp-32)/1.8
+        elif to == 'K':
+            return (temp-32)*5/9 + 273.15
 
-test_get_newton_sqrt()
+def test_get_temp():
+    assert (round(get_temp(300,'K','C'),2))==26.85
+    assert (round(get_temp(16, 'C', 'F'), 2)) == 60.8
+    assert (round(get_temp(45, 'F', 'K'), 2)) == 280.37
+    
+test_get_temp()
+
+
+
+
+
